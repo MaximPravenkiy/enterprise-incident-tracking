ac7cf4ba7d095ac2a13cbd042c1ca323cbb60b82
Maxim <maksik.pravekiy@gmail.com>
2021-03-21 17:08:01 +0300

    the list component is replaced with a table

--- Frontend/src/components/Main/Incidents/Incidents.tsx
+++ Frontend/src/components/Main/Incidents/Incidents.tsx
@@ -1,41 +1,82 @@
-import {List} from 'antd';
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import styled from 'styled-components';
-import IncidentsHeader from "./IncidentsHeader/IncidentsHeader";
-import IncidentsItem from "./IncidentsItem/IncidentsItem";
-import LoadMoreButton from "./LoadMoreButton/LoadMoreButton";
 import {getIncidents} from "../../../redux/store/actions/incidentsCreator";
+import { Table, Space } from 'antd';
+import styled from "styled-components";
 
-const ListEx = styled(List)`
+const TableCustom = styled(Table)`
     width: 95%;
-    border: 2px solid;
-    padding: 15px 20px;
 `
+
+const columns = [
+    {
+        title: 'Icon',
+        dataIndex: 'icon',
+        key: 'icon',
+    },
+    {
+        title: 'IncidentName',
+        dataIndex: 'incidentName',
+        key: 'incidentName',
+    },
+    {
+        title: 'Description',
+        dataIndex: 'description',
+        key: 'description',
+    },
+    {
+        title: 'Assignee',
+        dataIndex: 'assignee',
+        key: 'assignee',
+    },
+    {
+        title: 'Area',
+        dataIndex: 'area',
+        key: 'area',
+    },
+    {
+        title: 'StartDate',
+        dataIndex: 'startDate',
+        key: 'startDate',
+    },
+    {
+        title: 'DueDate',
+        dataIndex: 'dueDate',
+        key: 'dueDate',
+    },
+    {
+        title: 'Priority',
+        dataIndex: 'priority',
+        key: 'priority',
+    },
+    {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: () => (
+            <Space size="middle">
+                <a href='#top'>Edit</a>
+                <a href='#top'>Delete</a>
+            </Space>
+        ),
+    },
+];
 
 const Incidents = () => {
     const dispatch = useDispatch();
-    const {initLoading, loading, listOfIncidents} = useSelector(({incidentsReducer}: any) => incidentsReducer);
-    const loadMore = !initLoading && !loading ? <LoadMoreButton/> : null;
+    const {listOfIncidents} = useSelector(({incidentsReducer}: any) => incidentsReducer);
+    // const loadMore = !initLoading && !loading ? <LoadMoreButton/> : null;
 
     useEffect(() => {
         dispatch(getIncidents());
-    }, []);
-
+    }, [dispatch]);
+    //
     return (
-        <>
-            <ListEx
-                header={<IncidentsHeader/>}
-                className="demo-loadmore-list"
-                loading={initLoading}
-                itemLayout="horizontal"
-                loadMore={loadMore}
-                dataSource={listOfIncidents}
-                renderItem={(incident) => (
-                    <IncidentsItem incident={incident}/>
-                )}
-            />
-        </>
+        <TableCustom columns={columns} dataSource={listOfIncidents} />
     );
 }
 
