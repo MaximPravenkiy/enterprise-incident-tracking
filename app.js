71d9b5e40ef4910a3b1fcb9da5743c6cf2543f36
Maxim <maksik.pravekiy@gmail.com>
2021-04-06 21:58:55 +0300

    fixed path

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('Frontend/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../Frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'));
     });
 }
 
